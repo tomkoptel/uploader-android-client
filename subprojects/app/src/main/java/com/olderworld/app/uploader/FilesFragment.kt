8c9946479521124641fa628040d12927b647bfc1
Tom Koptel <tom.koptel@gmail.com>
2020-10-04 22:22:32 +0200

    Clean up FilesFragment.kt

--- subprojects/app/src/main/java/com/olderworld/app/uploader/FilesFragment.kt
+++ subprojects/app/src/main/java/com/olderworld/app/uploader/FilesFragment.kt
@@ -1,28 +1,16 @@
 package com.olderworld.app.uploader
 
 import android.app.Activity
-import android.content.ContentResolver
 import android.content.Intent
 import android.content.collectUris
-import android.database.Cursor
-import android.net.Uri
 import android.os.Bundle
-import android.provider.OpenableColumns
-import android.text.format.Formatter
 import android.view.View
 import android.widget.ArrayAdapter
-import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.jakewharton.byteunits.DecimalByteUnit
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.content_main.view.*
-import kotlinx.coroutines.launch
 import timber.log.Timber
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
 
 @AndroidEntryPoint
 internal class FilesFragment : Fragment(R.layout.content_main) {
@@ -44,42 +32,8 @@ internal class FilesFragment : Fragment(R.layout.content_main) {
         }
 
         view.pickFiles?.setOnClickListener {
-            val packageManager = requireActivity().packageManager
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-                addCategory(Intent.CATEGORY_OPENABLE)
-                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
-                type = "*/*"
-
-                // TODO consider poiniting to the more friendly location e.g. downloads
-                // Optionally, specify a URI for the directory that should be opened in
-                // the system file picker before your app creates the document.
-                // putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
-            }
-            val component = intent.resolveActivity(packageManager)
-            if (component == null) {
-                Timber.d("Can not resolve intent=$intent")
-                // so sad
-            } else {
-                // TODO convert to the modern way to handle events
-                startActivityForResult(intent, PICK_FILE)
-            }
+            selectFilesForUpload()
         }
-    }
-
-    private fun updateUIState(
-        state: FilesViewModel.State?,
-        adapter: ArrayAdapter<String>
-    ) {
-        when (state) {
-            is FilesViewModel.State.NoUploads -> {
-                adapter.add("No active uploads")
-            }
-            is FilesViewModel.State.ActiveUploads -> {
-                adapter.clear()
-                adapter.addAll(state.asStringList)
-            }
-        }
-        adapter.notifyDataSetChanged()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -102,103 +56,41 @@ internal class FilesFragment : Fragment(R.layout.content_main) {
         }
     }
 
-    private fun dumpUri(fileUri: Uri) {
-        // argh, Android why?
-        val context = requireActivity()
-        val contentResolver = context.contentResolver
-        contentResolver.takePersistableUriPermission(
-            fileUri,
-            Intent.FLAG_GRANT_READ_URI_PERMISSION
-        )
+    private fun selectFilesForUpload() {
+        val packageManager = requireActivity().packageManager
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            type = "*/*"
 
-        Intent(
-            BuildConfig.ACTION_UPLOAD,
-            fileUri,
-            context,
-            FileUploaderService::class.java
-        ).let { context.startService(it) }
-
-        DocumentFile.fromSingleUri(context, fileUri)?.let { documentFile ->
-            documentFile.timberDebugInfo()
-
-            when {
-                documentFile.isFile -> {
-                    documentFile.readCursor(contentResolver) {
-                        val sizeIndex = getColumnIndex(OpenableColumns.SIZE)
-                        moveToFirst()
-                        val size = getLong(sizeIndex)
-                        if (isNull(sizeIndex)) {
-                            Timber.d("So the size is not accessible :(")
-                        } else {
-                            val humanSize = Formatter.formatFileSize(context, size)
-                            val sizeInMB =
-                                DecimalByteUnit.MEGABYTES.convert(size, DecimalByteUnit.BYTES)
-                            val isFileUnder10mb = sizeInMB <= 10
-                            Timber.d("So the size is $humanSize isFileUnder10mb=$isFileUnder10mb")
-                        }
-                    }
-
-                    // TODO it is probably better idea to safe file first to the cache and then upload it ?
-                    // https://developer.android.com/training/data-storage/app-specific#query-free-space
-                    viewLifecycleOwner.lifecycleScope.launch {
-                        try {
-                            val text = documentFile.readAsText(contentResolver)
-                            val printTrimValue = 10.coerceAtMost(text.length)
-                            val textTrim = text.substring(0, printTrimValue)
-                            Timber.d("First 10 chars of file \n $textTrim")
-                        } catch (ex: IOException) {
-                            Timber.e("Can not read file content as string")
-                        }
-                    }
-                }
-                documentFile.isVirtual -> {
-                    // TODO handle as virtual https://developer.android.com/training/data-storage/shared/documents-files#open-virtual-file
-                }
-                else -> {
-                    Timber.d("document=${documentFile.uri} not a file ignore")
-                }
-            }
+            // TODO consider poiniting to the more friendly location e.g. downloads
+            // Optionally, specify a URI for the directory that should be opened in
+            // the system file picker before your app creates the document.
+            // putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri)
+        }
+        val component = intent.resolveActivity(packageManager)
+        if (component == null) {
+            Timber.d("Can not resolve intent=$intent")
+            // so sad
+        } else {
+            // TODO convert to the modern way to handle events
+            startActivityForResult(intent, PICK_FILE)
         }
     }
 
-    private fun DocumentFile.readCursor(
-        contentResolver: ContentResolver,
-        accessor: Cursor.() -> Unit
+    private fun updateUIState(
+        state: FilesViewModel.State?,
+        adapter: ArrayAdapter<String>
     ) {
-        contentResolver.query(uri, null, null, null, null)?.use(accessor)
-    }
-
-    private fun DocumentFile.readAsText(contentResolver: ContentResolver): String {
-        val stringBuilder = StringBuilder()
-        contentResolver.openInputStream(uri)?.use { inputStream ->
-            BufferedReader(InputStreamReader(inputStream)).use { reader ->
-                var line: String? = reader.readLine()
-                while (line != null) {
-                    stringBuilder.append(line)
-                    line = reader.readLine()
-                }
+        when (state) {
+            is FilesViewModel.State.NoUploads -> {
+                adapter.add("No active uploads")
+            }
+            is FilesViewModel.State.ActiveUploads -> {
+                adapter.clear()
+                adapter.addAll(state.asStringList)
             }
         }
-        return stringBuilder.toString()
-    }
-
-
-    private fun DocumentFile.timberDebugInfo() {
-        val directory = this.isDirectory
-        val isFile = this.isFile
-        val isVirtual = this.isVirtual
-        val name = this.name
-        val canRead = this.canRead()
-        val uri = this.uri
-        Timber.d(
-            """
-                    directory=$directory,
-                    isFile=$isFile,
-                    isVirtual=$isVirtual
-                    name=$name
-                    canRead=$canRead
-                    uri=$uri
-                """.trimIndent()
-        )
+        adapter.notifyDataSetChanged()
     }
 }
